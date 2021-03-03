6532d6737c397b9a4c8d25aea42e9477a2becd05
Marcelo Felix <celofelix93@gmail.com>
2021-03-03 15:02:30 -0300

    Apenas extraido o código de testes de object expressions

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,19 +1,9 @@
-import br.com.alura.bytebank.modelos.Cliente
-import br.com.alura.bytebank.modelos.Conta
-import br.com.alura.bytebank.modelos.ContaCorrente
-import br.com.alura.bytebank.modelos.ContaPoupanca
-
 fun main() {
 
-    val alex = Cliente(nome = "Alex", CPF = "", senha = 1)
-    val contaPoupanca = ContaPoupanca(titular = alex, numero = 1)
-    val contaCorrente = ContaCorrente(titular = alex, numero = 2)
-
-    testaContasDiferentes()
-
-    println("Total de contas: ${Conta.total}")
-
 }
+
+
+
 
 
 
