fc76b6d50f886df4a103005c373286f9346875f2
Marcelo Felix <celofelix93@gmail.com>
2021-03-04 10:41:30 -0300

    Testando o try e catch para captura de exceptions

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -2,6 +2,33 @@ import br.com.alura.bytebank.modelos.Endereco
 
 fun main() {
 
+    println("início main")
+    funcao1()
+    println("fim main")
+}
+
+fun funcao1(){
+    println("início funcao1")
+    try {
+        funcao2()
+    } catch (e: ClassCastException) {
+        e.printStackTrace()
+        println("ClassCastException capturada pelo catch")
+    }
+    println("fim funcao1")
+}
+
+fun funcao2() {
+    println("início funcao2")
+
+        for (i in 1..5){
+            println(i)
+
+            val endereco = Any()
+            endereco as Endereco
+        }
+    println("fim funcao2")
+
 }
 
 
