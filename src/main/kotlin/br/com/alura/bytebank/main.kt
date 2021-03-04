21b392ab464ce1aaaa35f36f539bed212ee72ce0
Marcelo Felix <celofelix93@gmail.com>
2021-03-04 15:51:02 -0300

    Testando o operador de safe call '?' para teste de valor nulo, caso ao acessar um membro de uma classe que seja nulo o safe call irá devolver um null sem quebrar o programa. Para fazer chamadas encadeadas de outros métodos também é necessários usar o operador de safe call ?. Também testando a função let{} que também é uma forma de fazer safe call

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -2,9 +2,22 @@ import br.com.alura.bytebank.modelos.Endereco
 
 fun main() {
 
-    var enderecoNulo: Endereco? = null
-    val enderecoNaoNulo: Endereco = enderecoNulo!!
-    println("${enderecoNaoNulo.logradouro}")
+    var enderecoNulo: Endereco? = Endereco(logradouro = "Rua não nula")
+    
+    val logradouroNovo: String? = enderecoNulo?.logradouro
+    println(logradouroNovo?.length)
+
+    enderecoNulo?.let {it ->
+        println(it.logradouro.length)
+    }
+
+//    Testando uma variavel com tipo e tornando ela como Nullable para aceitar valor null
+//    var enderecoNulo: Endereco? = null
+//    Usando o operador assert non-null !!.
+//    Dessa forma estamos garantindo que a variavel que está sendo atribuída não é nulla
+//    Mas como nesse caso ela está nulla o programa irá quebrar
+//    val enderecoNaoNulo: Endereco = enderecoNulo!!
+//    println("${enderecoNaoNulo.logradouro}")
 }
 
 
