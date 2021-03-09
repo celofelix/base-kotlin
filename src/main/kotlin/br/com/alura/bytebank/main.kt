8ab88cfe1a3f898e7d5681525e85732c8fc45ec0
Marcelo Felix <celofelix93@gmail.com>
2021-03-09 13:11:00 -0300

    Testando conceito de higher order function com as funções let{} e filter{}

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,8 +1,24 @@
+import br.com.alura.bytebank.modelos.Endereco
 import br.com.alura.bytebank.teste.testaFuncaoAnonima
 import br.com.alura.bytebank.teste.testaFuncaoLambda
 
 fun main() {
-    
+
+//    val endereco = Endereco(logradouro = "rua vergueiro", numero = 3185)
+//    val enderecoEmMaiusculo = "${endereco.logradouro}, ${endereco.numero}".toUpperCase()
+//    println(enderecoEmMaiusculo)
+
+    Endereco(logradouro = "rua vergueiro", numero = 3185).let { endereco ->
+        "${endereco.logradouro}, ${endereco.numero}".toUpperCase()
+    }.let(::println)
+
+    listOf(
+        Endereco(complemento = "Casa"),
+        Endereco(),
+        Endereco(complemento = "Apartamento")
+    ).filter { endereco ->  endereco.complemento.isNotEmpty()}
+        .let (::println)
+
 }
 
 
