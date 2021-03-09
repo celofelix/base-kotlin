ebd3b8f5d50fd0e47ba620ac5e57ba1e41db9311
Marcelo Felix <celofelix93@gmail.com>
2021-03-09 10:39:24 -0300

    Testando variavel do tipo função com referência para um funçaõ e classes como função sobrescrevendo o invoke(), também testando função lambdas e funções anônimas

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,6 +1,46 @@
+import br.com.alura.bytebank.teste.testaNullableESafeCall
+
 fun main() {
 
+    val minhaFuncaoLambda: () -> Unit = {
+        println("Executa como lambda")
+    }
+    println("Função Lambda: ${minhaFuncaoLambda()}")
+
+    val minhaFuncaoAnonima: () -> Unit = fun() {
+        println("Excuta Função anonima")
+    }
+    println("Função anonima: ${minhaFuncaoAnonima()}")
 }
+
+fun testaFuncaoClasse() {
+    val minhaFuncaoClasse: () -> Unit = Teste()
+    println(minhaFuncaoClasse())
+}
+
+fun testaFuncaoReferencia() {
+    val adicao: (Int, Int) -> Int = ::soma
+    println(adicao)
+    val total = adicao(3, 5)
+    println(total)
+
+    val funcaoTeste: () -> Unit = ::teste
+    println(funcaoTeste())
+}
+
+fun teste() {
+    println("Executa função teste")
+}
+
+fun soma(a: Int, b: Int): Int = a + b
+
+class Teste: () -> Unit {
+    override fun invoke() {
+        println("Executa invoke da classe Teste")
+    }
+
+}
+
 
 
 
